71d1a36d4ec575d942f055c3ad18976d7bca832d
out-millennium <z95biaka@gmail.com>
2026-02-06 00:37:09 +0400

    Create db.py

---
+++ core/db.py
@@ -0,0 +1,25 @@
+# core/db.py
+import sqlite3
+
+conn = sqlite3.connect("ledger.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS accounts (
+    account_id TEXT PRIMARY KEY,
+    balance INTEGER NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ledger (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    operation TEXT NOT NULL,
+    from_account TEXT,
+    to_account TEXT,
+    amount INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+""")
+
+conn.commit()
